1b22fae80cc739524fb2e295f166fcd55cf92eb3
ThomasDuvinage <toto.duvinage@gmail.com>
2025-02-07 16:53:00 +0900

    [controllers] add option to compile independently controllers

--- controllers/baseline_controllers.cmake
+++ controllers/baseline_controllers.cmake
@@ -1,19 +1,26 @@
-include(${CMAKE_CURRENT_LIST_DIR}/CentroidalControlCollection.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/BaselineFootstepPlanner.cmake)
 
-AddProject(BaselineWalkingController
-  GITHUB isri-aist/BaselineWalkingController
-  GIT_TAG origin/master
-  DEPENDS CentroidalControlCollection BaselineFootstepPlanner ForceControlCollection TrajectoryCollection
-)
+if(${WITH_MULTI_CONTACT_CONTROLLER} OR ${WITH_BASELINE_WALKING_CONTROLLER} OR  ${WITH_LOCOMANIP_MANIPULATOR_CONTROLLER})
+  include(${CMAKE_CURRENT_LIST_DIR}/CentroidalControlCollection.cmake)
+  include(${CMAKE_CURRENT_LIST_DIR}/BaselineFootstepPlanner.cmake)
 
-AddProject(MultiContactController
-  GITHUB isri-aist/MultiContactController
-  GIT_TAG origin/master
-  DEPENDS BaselineWalkingController
-)
+  AddProject(BaselineWalkingController
+    GITHUB isri-aist/BaselineWalkingController
+    GIT_TAG origin/master
+    DEPENDS CentroidalControlCollection BaselineFootstepPlanner ForceControlCollection TrajectoryCollection
+  )
 
-#AddProject(LocomanipController
-#  GITHUB isri-aist/LocomanipController
-#  GIT_TAG origin/master
-#  DEPENDS BaselineWalkingController CentroidalControlCollection ForceControlCollection)
+  if(WITH_MULTI_CONTACT_CONTROLLER)
+    AddProject(MultiContactController
+      GITHUB isri-aist/MultiContactController
+      GIT_TAG origin/master
+      DEPENDS BaselineWalkingController
+    )
+  endif()
+
+  if(WITH_LOCOMANIP_MANIPULATOR_CONTROLLER)
+    #AddProject(LocomanipController
+    #  GITHUB isri-aist/LocomanipController
+    #  GIT_TAG origin/master
+    #  DEPENDS BaselineWalkingController CentroidalControlCollection ForceControlCollection)
+  endif()
+endif()
